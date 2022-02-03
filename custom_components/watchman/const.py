52a7750543e8bf619805aff33d448e54bc85a82e
scratcha <scratcha@gmail.com>
2022-02-03 19:02:55 +0300

    initial codebase

---
+++ custom_components/watchman/const.py
@@ -0,0 +1,22 @@
+DOMAIN = "watchman"
+
+DEFAULT_REPORT_FILENAME = "thewatchman_report.txt"
+DEFAULT_HEADER = "-== WATCHMAN REPORT ==- "
+
+CONF_IGNORED_FILES = "ignored_files"
+CONF_HEADER = "header"
+CONF_REPORT_PATH = "report_path"
+CONF_IGNORED_ITEMS = "ignored_items"
+CONF_SERVICE_NAME = "service"
+CONF_SERVICE_DATA = "data"
+CONF_INCLUDED_FOLDERS = "included_folders"
+CONF_CHECK_LOVELACE = "check_lovelace"
+CONF_IGNORED_STATES = "ignored_states"
+CONF_CHUNK_SIZE = "chunk_size"
+CONF_CREATE_FILE = "create_file"
+CONF_SEND_NITIFICATION = "send_notification"
+CONF_PARSE_CONFIG = "parse_config"
+CONF_COLUMNS_WIDTH = "columns_width"
+
+EVENT_AUTOMATION_RELOADED = "automation_reloaded"
+EVENT_SCENE_RELOADED = "scene_reloaded"
