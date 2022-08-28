8f73cf0b26c365a9d6c17758f28ae398ef9cc335
Yuval Aboulafia <yuval.abou@gmail.com>
2022-08-28 20:59:12 +0300

    format

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,9 @@ pytest_plugins = "pytest_homeassistant_custom_component"  # pylint: disable=inva
 # This fixture enables loading custom integrations in all tests.
 # Remove to enable selective use of this fixture
 @pytest.fixture(autouse=True)
-def auto_enable_custom_integrations(enable_custom_integrations): # pylint: disable=unused-argument
+def auto_enable_custom_integrations(
+    enable_custom_integrations,
+):  # pylint: disable=unused-argument
     """Automatically enable loading custom integrations in all tests."""
     yield
 
