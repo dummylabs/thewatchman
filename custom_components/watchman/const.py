22392be15a708dac13d8316b6febb53f1b1aac4e
dummylabs <scratcha@gmail.com>
2024-11-25 04:49:25 +0000

    bump version

--- custom_components/watchman/const.py
+++ custom_components/watchman/const.py
@@ -4,7 +4,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "watchman"
 DOMAIN_DATA = "watchman_data"
-VERSION = "0.6.4"
+VERSION = "0.6.5"
 
 CONFIG_ENTRY_VERSION = 2
 CONFIG_ENTRY_MINOR_VERSION = 1
